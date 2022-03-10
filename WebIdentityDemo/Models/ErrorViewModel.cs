6cb36966c580514ac84cf4ed896d512e6123a52e
Mehrdad <mehzan07@yahoo.com>
2022-03-10 23:07:17 +0100

    Add project files.

---
+++ WebIdentityDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebIdentityDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
