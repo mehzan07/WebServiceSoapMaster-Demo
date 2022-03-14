dd07f3c34fab0805296b5c69499fade7053d2b0b
Mehrdad <mehzan07@yahoo.com>
2022-03-14 16:01:32 +0100

    changing connection string

---
+++ WebIdentityDemo/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using WebIdentityDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using WebServiceSoapDemo;
+
+namespace WebIdentityDemo.Repository
+{
+   public interface IRepository
+    {
+
+        //Task<SoapDemoSoapClient> GetInstanceAsync();
+        Task<Response<IdentityModel>> LoginAsync(LoginViewModel loginView);
+         Task<Response<string>> RegisterAsync(RegisterViewModel registerView);
+
+    }
+}
